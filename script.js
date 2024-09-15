588fdcadee1b0d411866fa213e7055e16cdbc764
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-15 20:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+  function getDomLevel(element) {
+        let level = 1; 
+        while (element.parentElement) {
+            element = element.parentElement;
+            level++;
+        }
+        
+        return level;
+    }
+   
+const element = document.getElementById('level');
+
+	if (element) {
+        const domLevel = getDomLevel(element);
+        alert(`The level of the element is: ${domLevel}`);
+    } else {
+        alert("Element with ID 'level' not found.");
+    }
